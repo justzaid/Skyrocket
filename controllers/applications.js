d621401aa3df90e3a2bd37d068fea63e1a779520
justzxid <zaidmk96@gmail.com>
2025-02-04 13:18:42 +0300

    Implemented a list to show user current  applications

--- controllers/applications.js
+++ controllers/applications.js
@@ -7,18 +7,32 @@ const newApplication = (req, res) => {
 const createApplication = async (req, res) => {
     // user id = req.params.userId
     // user id = req.session.user._id
+    
+    try {
     const currentUser = await User.findById(req.params.userId)
     currentUser.applications.push(req.body)
     // Pushing the formData into our user model
     // Save our edits
     await currentUser.save()
     res.redirect(`/users/${currentUser._id}/applications`)
+    } catch (error) {
+        console.log(error)
+        res.redirect('/')
+    }
 }
 
-const index = (req, res) => {
+const index = async (req, res) => {
+    try{
+    const currentUser = await User.findById(req.params.userId)
+    console.log(currentUser.applications)
     res.render('applications/index.ejs',
-        {title: 'Your applications'}
-    )
+        {title: 'Your applications',
+        applications: currentUser.applications    
+    })
+    } catch (error) {
+    console.log(error)
+    res.redirect('/')
+    }
 }
 
 module.exports = {
